f2faa96830f6309c3dea8d538572c66651d0a80e
Rob Cranfill <robcranfill@robcranfill.net>
2021-01-24 18:44:43 -0800

    Fixed links

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 Useful lib for using the Adafruit Raspberry Pi TFT LCD display.
 
 This is an object-oriented wrapper (such as it is) around various bits of code to make
-it easier to use the (Adafruit 1.3" TFT LCD display)[https://www.adafruit.com/product/4484] for the Raspberry Pi, in Python3.
+it easier to use the [Adafruit 1.3" TFT LCD display](https://www.adafruit.com/product/4484) for the Raspberry Pi, in Python3.
 
 For now, it only supports the 1.3" display, st7789, 240x240 (but it should be pretty easy
   to modify it for the other similar Adafruit displays).
 
-For much info on this cool little part, see (Adafruit)[https://learn.adafruit.com/adafruit-mini-pitft-135x240-color-tft-add-on-for-raspberry-pi].
+For much info on this cool little part, see [Adafruit](https://learn.adafruit.com/adafruit-mini-pitft-135x240-color-tft-add-on-for-raspberry-pi).
 
 
 @author robcranfill@robcranfill.net
 
-For the drawing methods we pass through, see [https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html]
+For the drawing methods we pass through, see (https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html)
--- testCranpitft.py
+++ testCranpitft.py
@@ -3,34 +3,37 @@
 from CranPiTFT import CranPiTFT
 import time
 
-c = CranPiTFT()
+c = CranPiTFT(rotation=180)
 # print(c)
 # c.makeAnX()
 
 h = c.height
 w = c.width
 
-for iter in range(10):
+try:
+    for iter in range(10):
 
-    # Using object methods to draw:
-    c.line([(0, 0), (w, h)], fill=(255, 0, 0), width=4)
-    c.line([(0, h), (w, 0)], fill=(255, 0, 0), width=4)
+        # Using object methods to draw:
+        c.line([(0, 0), (w, h)], fill=(255, 0, 0), width=4)
+        c.line([(0, h), (w, 0)], fill=(255, 0, 0), width=4)
 
-    balls = iter+1
-    k = w/balls
-    for i in range(balls):
-        c.ellipse([i*k, i*k, (i+1)*k, (i+1)*k], fill=(int(255*i/balls), 0, int(255*i/balls)))
+        balls = iter+1
+        k = w/balls
+        for i in range(balls):
+            c.ellipse([i*k, i*k, (i+1)*k, (i+1)*k], fill=(int(256*i/balls), 0, 256-int(256*i/balls)))
 
-    c.updateImage()
+        c.updateImage()
 
-    # Using direct drawing on the ImageDraw object:
-    draw = c.draw
-    draw.rectangle((50, h-50, 100, h-100), fill=(255,0,0))
-    draw.rectangle((w-50, 50, w-100, 100), fill="#FF00FF")
+        # Using direct drawing on the ImageDraw object:
+        draw = c.draw
+        draw.rectangle((50, h-50, 100, h-100), fill=(255,0,0))
+        draw.rectangle((w-50, 50, w-100, 100), fill="#FF00FF")
 
-    c.updateImage()
+        c.updateImage()
 
-    time.sleep(1)
+        time.sleep(1)
+        c.clearToBlack()
+except:
     c.clearToBlack()
-
+    c.setBacklight(False)
 print("Done")
